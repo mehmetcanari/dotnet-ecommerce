6a43265024971a344431bb56cf638e7e3b7ec54f
Mehmet Can Ari <bsn.mehmetcanari@gmail.com>
2025-04-24 19:19:00 +0300

    refactor: Improve validation rules in AccountUpdateValidation class for enhanced validation logic

--- ECommerce.Application/Validations/Account/AccountUpdateValidation.cs
+++ ECommerce.Application/Validations/Account/AccountUpdateValidation.cs
@@ -8,29 +8,26 @@ public class AccountUpdateValidation : AbstractValidator<AccountUpdateRequestDto
     public AccountUpdateValidation()
     {
         RuleFor(x => x.Name)
-            .Length(2, 50)
-            .NotEmpty()
-            .WithMessage("Full name is required");
-        
+            .NotEmpty().WithMessage("Name is required")
+            .Length(2, 50).WithMessage("Name must be between 2 and 50 characters");
+
         RuleFor(x => x.Email)
-            .EmailAddress()
-            .NotEmpty()
-            .WithMessage("Email is required");
-        
+            .NotEmpty().WithMessage("Email is required")
+            .EmailAddress().WithMessage("Invalid email format")
+            .MaximumLength(100).WithMessage("Email must not exceed 100 characters");
+
         RuleFor(x => x.Password)
-            .Length(6, 50)
-            .NotEmpty()
-            .WithMessage("Password is required");
-        
+            .NotEmpty().WithMessage("Password is required")
+            .MinimumLength(6).WithMessage("Password must be at least 6 characters")
+            .MaximumLength(50).WithMessage("Password must not exceed 50 characters");
+
         RuleFor(x => x.Address)
-            .Length(2, 100)
-            .NotEmpty()
-            .WithMessage("Address is required");
-        
+            .NotEmpty().WithMessage("Address is required")
+            .Length(2, 100).WithMessage("Address must be between 2 and 100 characters");
+
         RuleFor(x => x.PhoneNumber)
-            .NotEmpty()
-            .WithMessage("Phone number is required")
-            .Length(10,10)
-            .WithMessage("Phone number must be 10 digits");
+            .NotEmpty().WithMessage("Phone number is required")
+            .Length(10).WithMessage("Phone number must be 10 digits")
+            .Matches(@"^[0-9]+$").WithMessage("Phone number must contain only numbers");
     }
 }
